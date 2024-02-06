2c908e7c39571d75ec9b6dd957afce7cc25817d2
LineCab <line.cabaret@mmibordeaux.com>
2024-02-06 10:31:38 +0100

    commit init

---
+++ views/backend/statuts/edit.php
@@ -0,0 +1,30 @@
+<?php
+include '../../../header.php';
+
+if(isset($_GET['numStat'])){
+    $numStat = $_GET['numStat'];
+    $libStat = sql_select("STATUT", "libStat", "numStat = $numStat")[0]['libStat'];
+}
+?>
+
+<!-- Bootstrap form to create a new statut -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Modifier un Statut</h1>
+        </div>
+        <div class="col-md-12">
+            <!-- Form to create a new statut -->
+            <form action="<?php echo ROOT_URL . "/api/statuts/update.php?numStat=" . $numStat ?>" method="post">
+                <div class="form-group">
+                    <label for="libStat">Nom du statut</label>
+                    <input id="libStat" name="libStat" class="form-control" type="text" autofocus="autofocus" value="<?php echo $libStat; ?>"/>
+                </div>
+                <br />
+                <div class="form-group mt-2">
+                    <button type="submit" class="btn btn-primary">Confirmer update ?</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
