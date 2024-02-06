983ab9f496d1a70d16f5bbd9c63ed4bccd803ff6
Gabriel Bisson--Barret <gabriel.bisson--barret@mmibordeaux.com>
2024-02-06 17:49:17 +0100

    ajt date create comm

--- api/comments/create.php
+++ api/comments/create.php
@@ -3,8 +3,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once '../../functions/ctrlSaisies.php';
 
 $libCom = ctrlSaisies($_POST['libCom']);
+$date = date('Y-m-d H:i:s');
 
-sql_insert('comment', 'libCom', "'$libCom'");
-
+sql_insert('comment', 'libCom','dtCreaCom', "'$libCom'", "'$date");
 
 header('Location: ../../views/backend/comments/list.php');
