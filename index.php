8c1b10511c317b9445ffb95eb167cdf5ae1dce3a
LineCab <line.cabaret@mmibordeaux.com>
2024-02-07 14:37:52 +0100

    rien de spé

--- index.php
+++ index.php
@@ -28,11 +28,6 @@ require_once 'config.php';
 ?>
 
 <body>
-    <div id="cookieConsent" class="cookie-consent">
-        <p>Ce site utilise des cookies pour améliorer l'expérience utilisateur. En continuant à naviguer sur ce site, vous acceptez notre utilisation des cookies.</p>
-        <button id="acceptCookiesBtn">Accepter</button>
-        <button id="refuseCookiesBtn">Refuser</button>
-    </div>
     <hr class="my-3">
     <h2 class="uppercase"> L'actualité </h2>
     <hr class="my-3">
@@ -171,69 +166,3 @@ require_once 'config.php';
 </body>
 
 <?php require_once 'footer.php'; ?>
-
-<script>
-
-// Fonction pour définir un cookie
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-// Fonction pour afficher la pop-up de consentement des cookies
-function showCookieConsent() {
-    var cookieConsent = document.getElementById('cookieConsent');
-    cookieConsent.style.display = 'block';
-}
-
-// Fonction pour cacher la pop-up de consentement des cookies
-function hideCookieConsent() {
-    var cookieConsent = document.getElementById('cookieConsent');
-    cookieConsent.style.display = 'none';
-}
-
-// Vérifier si l'utilisateur a déjà accepté les cookies
-function checkCookieConsent() {
-    var consentCookie = getCookie('cookieConsent');
-    if (consentCookie === 'accepted') {
-        hideCookieConsent();
-    } else {
-        showCookieConsent();
-    }
-}
-
-// Vérifier si un cookie existe
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-// Événement pour accepter les cookies
-document.getElementById('acceptCookiesBtn').addEventListener('click', function() {
-    setCookie('cookieConsent', 'accepted', 365); // Accepter les cookies pour une année
-    hideCookieConsent();
-});
-
-// Événement pour refuser les cookies
-document.getElementById('refuseCookiesBtn').addEventListener('click', function() {
-    setCookie('cookieConsent', 'refused', 365); // Refuser les cookies pour une année
-    hideCookieConsent();
-});
-
-// Appeler la fonction de vérification des cookies lors du chargement de la page
-window.onload = checkCookieConsent;
-
-</script>
