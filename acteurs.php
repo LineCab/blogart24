5e7505c8a6a414599e0c9d891418277f3869aaa5
LineCab <line.cabaret@mmibordeaux.com>
2024-02-08 12:48:12 +0100

    Page events et acteurs

--- acteurs.php
+++ acteurs.php
@@ -1,11 +1,9 @@
 <?php 
-    require_once 'header.php';
-    sql_connect();
+require_once 'header.php';
+sql_connect();
+
+$articles = sql_select("ARTICLE", "*", "numThem = 2");
 ?>
-
-<!DOCTYPE html>
-<html lang="fr-FR">
-
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -29,120 +27,38 @@ require_once 'config.php';
 
 <body>
     <div class="custom-column row row-cols-1 row-cols-md-2 g-4 margin-tb-80">
-        <div class="col">
-            <div class="card">
-                <img src="/src/images/bieres.png" height="400" class="card-img-top object-fit-cover" alt="Photo bière">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                        <p class="card-text card-date">10/05/2024</p>
-                        <div class="d-flex align-items-center like-com-articles">
-                            <img src="/src/images/coeur.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
-                            113
-                            <img src="/src/images/comment.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
-                            78
+        <?php
+        foreach($articles as $article){
+            $numArt = $article["numArt"];
+            $dtCreaArt = $article["dtCreaArt"];
+            $dtCreaArtFormat = date('d/m/Y', strtotime($dtCreaArt));
+            $urlImg = $article["urlPhotArt"];
+            $libTitrArt = $article["libTitrArt"];
+            $libChapoArt = $article["libChapoArt"];
+            $nbLike = sql_select("LIKEART", "COUNT(*) as nbLike", "numArt = $numArt AND likeA = 1");
+            $nbCom = sql_select("COMMENT", "COUNT(*) as nbLike", "numArt = $numArt");
+        ?>
+            <div class="col">
+                <div class="card">
+                    <img src="<?php echo "/src/uploads/".$urlImg ?>" height="400" class="card-img-top object-fit-cover">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between">
+                            <p class="card-text card-date"><?php echo $dtCreaArtFormat ?></p>
+                            <div class="d-flex align-items-center like-com-articles">
+                                <img src="/src/images/coeur.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
+                                <?php echo $nbLike[0]["nbLike"] ?>
+                                <img src="/src/images/comment.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
+                                <?php echo $nbCom[0]["nbLike"] ?>
+                            </div>
                         </div>
+                        <h3 class="card-title"><?php echo $libTitrArt ?></h3>
+                        <p class="card-text"><?php echo $libChapoArt ?></p>
                     </div>
-                    <h3 class="card-title">Card title</h3>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
                 </div>
             </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="/src/images/bieres.png" height="400" class="card-img-top object-fit-cover" alt="Photo bière">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                        <p class="card-text card-date">10/05/2024</p>
-                        <div class="d-flex align-items-center like-com-articles">
-                            <img src="/src/images/coeur.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
-                            113
-                            <img src="/src/images/comment.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
-                            78
-                        </div>
-                    </div>
-                    <h3 class="card-title">Card title</h3>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="/src/images/bieres.png" height="400" class="card-img-top object-fit-cover" alt="Photo bière">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                        <p class="card-text card-date">10/05/2024</p>
-                        <div class="d-flex align-items-center like-com-articles">
-                            <img src="/src/images/coeur.png" height="20" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
-                            113
-                            <img src="/src/images/comment.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
-                            78
-                        </div>
-                    </div>
-                    <h3 class="card-title">Card title</h3>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="/src/images/bieres.png" height="400" class="card-img-top object-fit-cover" alt="Photo bière">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                        <p class="card-text card-date">10/05/2024</p>
-                        <div class="d-flex align-items-center like-com-articles">
-                            <img src="/src/images/coeur.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
-                            113
-                            <img src="/src/images/comment.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
-                            78
-                        </div>
-                    </div>
-                    <h3 class="card-title">Card title</h3>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="/src/images/bieres.png" height="400" class="card-img-top object-fit-cover" alt="Photo bière">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                        <p class="card-text card-date">10/05/2024</p>
-                        <div class="d-flex align-items-center like-com-articles">
-                            <img src="/src/images/coeur.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
-                            113
-                            <img src="/src/images/comment.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
-                            78
-                        </div>
-                    </div>
-                    <h3 class="card-title">Card title</h3>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="/src/images/bieres.png" height="400" class="card-img-top object-fit-cover" alt="Photo bière">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                        <p class="card-text card-date">10/05/2024</p>
-                        <div class="d-flex align-items-center like-com-articles">
-                            <img src="/src/images/coeur.png" height="18" class="card-img-top object-fit-cover ml-2" alt="Photo bière">
-                            113
-                            <img src="/src/images/comment.png" height="18" class="card-img-top object-fit-cover mx-2" alt="Photo bière">
-                            78
-                        </div>
-                    </div>
-                    <h3 class="card-title">Card title</h3>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                </div>
-            </div>
-        </div>
+        <?php
+        }
+        ?>
     </div>
 </body>
 
