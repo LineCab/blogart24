ffc4d42709753f86795d5fd0f5a1f5cef70e3664
TyaMaziou <125308500+TyaMaziou@users.noreply.github.com>
2024-02-08 12:42:08 +0100

    y a r
    test php like mais au final pas de trie / like

--- index.php
+++ index.php
@@ -29,7 +29,6 @@ require_once 'config.php';
 $articleAct = sql_select("ARTICLE", "*",null,"dtCreaArt DESC", 2);
 $titreAct = sql_select("ARTICLE", "*", null,"libTitrArt", 2);
 $libChapoArt = sql_select("ARTICLE", "*", null,"libChapoArt", 2);
-// $articleAct = sql_select("ARTICLE", "*", null, "likeA DESC", 10);
 
 ?> 
 
@@ -78,6 +77,7 @@ $libChapoArt = sql_select("ARTICLE", "*", null,"libChapoArt", 2);
                     <h3 class="card-title">Card title</h3>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                         card's content.</p>
+    
                 </div>
             </div>
         </div>
