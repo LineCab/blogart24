a1dd2d0e7255af70550e449e9318e9b422debb1c
TyaMaziou <125308500+TyaMaziou@users.noreply.github.com>
2024-02-09 09:08:36 +0100

    cgu
    fini

--- views/frontend/CGU.php
+++ views/frontend/CGU.php
@@ -40,42 +40,99 @@
   <div class="col-8">
   <div data-bs-spy="scroll" data-bs-target="#list-example" data-bs-smooth-scroll="true" class="scrollspy-example" tabindex="0">
 
-    <h4 id="list-item-1">ARTICLE 1. Collecte des données personnelles : </h4>
-      <p>Le blog GARDENA, collecte des données personnelles telles que le nom, 
-        l'adresse e-mail et éventuellement d'autres informations fournies volontairement par les utilisateurs lorsqu'ils commentent des articles, s'abonnent à la newsletter, 
-        ou interagissent avec le blog de toute autre manière.</p>
-      
-      <h4 id="list-item-2">ARTICLE 2. Finalités du traitement : </h4>
-      <p>Les données personnelles collectées sont utilisées dans le but de permettre aux utilisateurs d'interagir avec le blog, de recevoir des mises à jour, de participer à des discussions, et de personnaliser leur expérience sur le site.</p>
-      
-      <h4 id="list-item-3">ARTICLE 3. Base légale du traitement : </h4>
-      <p>Le traitement des données personnelles repose sur le consentement des utilisateurs, qui est obtenu de manière explicite lors de la collecte des données.</p>
-      
-      <h4 id="list-item-4">ARTICLE 4. Sécurité des données : </h4>
-      <p>GARDENA prend des mesures de sécurité appropriées pour protéger les données personnelles des utilisateurs contre tout accès non autorisé, divulgation, altération ou destruction.</p>
-      
-      <h4 id="list-item-5">ARTICLE 5. Durée de conservation des données : </h4>
-      <p>Les données personnelles sont conservées aussi longtemps que nécessaire pour atteindre les finalités pour lesquelles elles ont été collectées, sauf si une période de conservation plus longue est requise par la loi.</p>
+    <p>
+    En vigueur au 1 Février 2024.
+    Les présentes conditions générales d'utilisation (dites « CGU ») ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par GARDENA et de définir les conditions d’accès et d’utilisation des services par « l'Utilisateur ».
+    <br>Les présentes CGU sont accessibles sur le site GARDENA à la rubrique «CGU».
+    <br>Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des présentes CGU par l’utilisateur. Lors de l'inscription sur le site via le Formulaire d’inscription, chaque utilisateur accepte expressément les présentes CGU en cochant la case précédant le texte suivant : « Je reconnais avoir lu et compris les CGU et je les accepte ».
+    <br>En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.
+    <br>GARDENA se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU.
+    </p>
+    <h4 id="list-item-1">ARTICLE 1 : Les mentions légales </h4>
+      <p>
+<br>L'édition du site GARDENA est assurée par la l’IUT Bordeaux Montaigne dont le siège social est situé au 1 Rue Jacques Ellul 33800 Bordeaux.
+Numéro de téléphone : 05 57 12 20 44
+<br>Adresse e-mail : accueil@u-bordeaux-montaigne.fr.
+<br>Les Directeurs de la publication sont : Madame CABARET Line, Madame GARBUIO-BINAUD Eva, Madame MAZIERE—HADAJI Tya, Monsieur BREART Brice, Monsieur BISSON-BARRET Gabriel.
+<br><br>
+L'hébergeur du site GARDENA est la société Netlify, dont le siège social est situé à San Francisco, Californie, Etats-Unis . Opérant dans le domaine du cloud computing, de l'informatique sans serveur et de la plate-forme en tant que service, Netlify compte parmi ses produits phares Netlify CMS et Jamstack.</p>
+     
+      <h4 id="list-item-2">ARTICLE 2 : Accès au site</h4>
+      <p>Le site GARDENA  permet à l'Utilisateur un accès gratuit aux services suivants :
+      Le site internet propose les services suivants :
+      Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.
+      L’Utilisateur non membre n'a pas accès aux services réservés. Pour cela, il doit s’inscrire en remplissant le formulaire. En acceptant de s’inscrire aux services réservés, l’Utilisateur membre s’engage à fournir des informations sincères et exactes concernant son état civil et ses coordonnées, notamment son adresse email.</p>
+      <p>Pour accéder aux services, l’Utilisateur doit ensuite s'identifier à l'aide de son identifiant et de son mot de passe qui lui seront communiqués après son inscription.
+      Tout Utilisateur membre régulièrement inscrit pourra également solliciter sa désinscription en se rendant à la page dédiée sur son espace personnel. Celle-ci sera effective dans un délai raisonnable.
+      </p>
+      <p>Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du site ou serveur et sous réserve de toute interruption ou modification en cas de maintenance, n'engage pas la responsabilité de GARDENA. Dans ces cas, l’Utilisateur accepte ainsi ne pas tenir rigueur à l’éditeur de toute interruption ou suspension de service, même sans préavis.
+      <br>L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse email de l’éditeur communiqué à l’ARTICLE 1.</p>
+          
+      <h4 id="list-item-3">ARTICLE 3 : Collecte des données</h4>
+      <p>Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.
+      <br>En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :
+      </p>
 
-      <h4 id="list-item-6">ARTICLE 6. Partage des données : </h4>
-      <p>Les données personnelles ne sont pas partagées avec des tiers sans le consentement préalable des utilisateurs, sauf lorsque cela est nécessaire pour répondre aux exigences légales.</p>
+      <h4 id="list-item-4">ARTICLE 4 : Modification des CGU </h4>
+      <P>GARDENA se réserve le droit de modifier les présentes CGU à tout moment. Les Utilisateurs seront informés de ces modifications par tout moyen jugé approprié. L'utilisation du site après modification des CGU vaut acceptation de ces dernières.</P>
 
-      <h4 id="list-item-7">ARTICLE 7. Droits des utilisateurs : </h4>
-      <p>Les utilisateurs ont le droit d'accéder à leurs données personnelles, de les rectifier, de les supprimer, de limiter leur traitement, de s'opposer au traitement, et de demander la portabilité de leurs données conformément aux dispositions du RGPD.</p>
+      <h4 id="list-item-5">ARTICLE 5 : Propriété intellectuelle </h4>
+      <P>Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.</P>
+      <P>La marque GARDEN’ART est une marque déposée par GARDENA. Toute représentation et/ou reproduction et/ou exploitation partielle ou totale de cette marque, de quelque nature que ce soit, est totalement prohibée.
+      <br>L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.</P>
+      <P>Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L335-2 et suivants du Code de la propriété intellectuelle.
+      <br>Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.</P>
 
-      <h4 id="list-item-8">aARTICLE 8. Cookies et technologies similaires : </h4>
-      <p>Le blog utilise des cookies et des technologies similaires pour améliorer l'expérience de navigation des utilisateurs. Les utilisateurs peuvent gérer leurs préférences relatives aux cookies via les paramètres de leur navigateur.</p>
+      <h4 id="list-item-6">ARTICLE 6 : Responsabilité  </h4>
+      <p>Les sources des informations diffusées sur le site GARDENA sont réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.
+      <br>Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle. Malgré des mises à jour régulières, le site GARDENA ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication. De même, le site ne peut être tenue responsable de l’utilisation et de l’interprétation de l’information contenue dans ce site.
+      </p>
+      <p>L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite. Il assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.
+      </p>
+      <p>
+      Le site GARDENA ne peut être tenu pour responsable d’éventuels virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une utilisation, à l’accès, ou au téléchargement provenant de ce site.
+      <br>La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.</p>
 
-      <h4 id="list-item-9">ARTICLE 9. Modifications de la politique de confidentialité : </h4>
-      <p>La présente politique de confidentialité peut être modifiée à tout moment, et les utilisateurs seront informés de ces modifications de manière appropriée.</p>
+      <h4 id="list-item-7">ARTICLE 7 : Liens hypertextes</h4>
+      <p>
+      Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces liens, il sortira du site GARDENA. Ce dernier n’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.</p>
+        
+      <h4 id="list-item-8">ARTICLE 8 : Cookies </h4>
+      <p>L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.
+      <br>Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de l’Utilisateur par votre navigateur et qui sont nécessaires à l’utilisation du site GARDENA. Les cookies ne contiennent pas d’information personnelle et ne peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent.</p>
+      <p>L’information contenue dans les cookies est utilisée pour améliorer le site GARDENA.
+      <br>En naviguant sur le site, L’Utilisateur les “acceptes”.
+      <br>L’Utilisateur doit toutefois donner son consentement quant à l’utilisation de certains cookies.
+      <br>A défaut d’acceptation, l’Utilisateur est informé que certaines fonctionnalités ou pages risquent de lui être refusées.
+      <br>L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.
+      </p>
 
-      <h4 id="list-item-10">ARTICLE 10. Contact : </h4>
-      <p>Pour toute question ou demande concernant la politique de confidentialité, les utilisateurs peuvent contacter tyamaziou@gmail.com .</p>
-   
-      <h4 id="list-item-11">ARTICLE 11. Contact : </h4>
-      <p>Pour toute question ou demande concernant la politique de confidentialité, les utilisateurs peuvent contacter tyamaziou@gmail.com .</p>
-   
-    </div>
+      <h4 id="list-item-9">ARTICLE 9 : Publication par l’Utilisateur</h4>
+      <p>Le site permet aux membres de publier les contenus suivants :
+      <br>Publication de commentaires.
+      <br>Dans ses publications, le membre s’engage à respecter les règles de la Netiquette (règles de bonne conduite de l’internet) et les règles de droit en vigueur.
+      <br>Le site peut exercer une modération sur les publications et se réserve le droit de refuser leur mise en ligne, sans avoir à s’en justifier auprès du membre.
+      </p>
+
+      <p>Le membre reste titulaire de l’intégralité de ses droits de propriété intellectuelle. Mais en publiant une publication sur le site, il cède à la société éditrice le droit non exclusif et gratuit de représenter, reproduire, adapter, modifier, diffuser et distribuer sa publication, directement ou par un tiers autorisé, dans le monde entier, sur tout support (numérique ou physique), pour la durée de la propriété intellectuelle. Le Membre cède notamment le droit d'utiliser sa publication sur internet et sur les réseaux de téléphonie mobile.
+      <br>La société éditrice s'engage à faire figurer le nom du membre à proximité de chaque utilisation de sa publication.</p>
+
+      <p>Tout contenu posté, publié, et mis en ligne par l'Utilisateur est de sa seule responsabilité. L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes et s’engage à respecter les règles d’utilisation du site. Tout recours en justice engagé par un tiers lésé contre le site GARDENA sera pris en charge par l'Utilisateur.
+      <br>Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimé ou modifié par le site GARDENA, sans préavis.
+      </p>
+
+      <h4 id="list-item-10">ARTICLE 10 : Droit applicable et juridiction compétente </h4>
+      <p>La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, les tribunaux français de la ville de BORDEAUX seront seuls compétents pour en connaître.
+      <br>Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux coordonnées inscrites à l’ARTICLE 1.
+      </p>
+
+      <h4 id="list-item-11">ARTICLE 11 : Proctection des données </h4>
+      <p>
+      <br>Dans le cadre de l'utilisation du site GARDENA, des données à caractère personnel peuvent être collectées et traitées conformément à la législation en vigueur. L'Utilisateur reconnaît que l'éditeur garantit la sécurité et la confidentialité de ces données, mettant en place des mesures appropriées.
+      <br>L'Utilisateur a le droit d'accéder, de rectifier, de supprimer et d'opposer ses données personnelles en contactant l'éditeur. Ces données ne seront pas divulguées à des tiers non autorisés, sauf en cas de consentement explicite de l'Utilisateur ou lorsque requis par la loi.
+      <br>En cas de violation de la sécurité des données, l'éditeur s'engage à informer l'Utilisateur conformément à la réglementation en vigueur. L'Utilisateur reconnaît avoir pris connaissance et accepté la politique de confidentialité et de protection des données du site GARDENA.</p>
+    
+      </div>
   </div>
 </div>
 
